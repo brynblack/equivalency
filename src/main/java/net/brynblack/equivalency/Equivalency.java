e220de3e43520800af2b911713c2ce25f9d88192
brynblack <49110391+brynblack@users.noreply.github.com>
2022-11-13 11:47:09 +1100

    Initial commit

---
+++ src/main/java/net/brynblack/equivalency/Equivalency.java
@@ -0,0 +1,13 @@
+package net.brynblack.equivalency;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Equivalency implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("Equivalency");
+
+	@Override
+	public void onInitialize(ModContainer mod) {}
+}
